e1c9d07b06a3a8ea1f34582b6a1583433f36ed67
Horace <horacebenjamin007@hotmail.com>
2024-02-21 22:33:34 +0000

    added wire navigate to back link in create projects

--- resources/views/livewire/projects/create-projects.blade.php
+++ resources/views/livewire/projects/create-projects.blade.php
@@ -52,7 +52,7 @@
                             </div>
                         </div>
                         <div class="mt-6 flex items-center justify-end gap-x-6 mb-5">
-                            <a href="{{ route('view-projects') }}" type="button" class="text-sm font-semibold leading-6 text-gray-900"><i class="fa-solid fa-arrow-left"></i></a>
+                            <a href="{{ route('view-projects') }}" wire:navigate type="button" class="text-sm font-semibold leading-6 text-gray-900"><i class="fa-solid fa-arrow-left"></i></a>
                             <button wire:click="clearForm" type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
                             <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
                             @if(session('success'))
