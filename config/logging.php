fb5d264e1bdba1cee533bdc24c371946db16352a
Horace <horacebenjamin007@hotmail.com>
2025-07-09 14:33:04 +0100

    - added a custom testing log

--- config/logging.php
+++ config/logging.php
@@ -58,6 +58,12 @@ return [
             'ignore_exceptions' => false,
         ],
 
+        'testing' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/testing.log'),
+            'level' => 'debug',
+        ],
+
         'single' => [
             'driver' => 'single',
             'path' => storage_path('logs/laravel.log'),
